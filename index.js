00883e1f1e990d47f5df5750bd9c8e74ffd17997
Nihir Zala <zalanihir8866@gmail.com>
2024-03-18 16:56:11 +0530

    update the index js

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
-const express = require('express');
-const urlRoute = require('./routes/url');
-const {connectToMongoDB} = require('./connect');
+const express = require("express");
+const { connectToMongoDB } = require("./connect");
+const urlRoute = require("./routes/url");
+const URL = require("./models/url");
+
 const app = express();
-const PORT = 8000;
-connectToMongoDB('mongodb://localhost:27017/shorturl')
-.then(()=>console.log('MongoDB connected'));
-app.use('/url',urlRoute);
-app.listen(PORT, () => console.log(`Server Started at PORT http://localhost:${PORT}`));
+const PORT = 8001;
+
+connectToMongoDB("mongodb://localhost:27017/short-url").then(() =>
+  console.log("Mongodb connected")
+);
+
+app.use(express.json());
+
+app.use("/url", urlRoute);
+
+app.get("/:shortId", async (req, res) => {
+  const shortId = req.params.shortId;
+  const entry = await URL.findOneAndUpdate(
+    {
+      shortId,
+    },
+    {
+      $push: {
+        visitHistory: {
+          timestamp: Date.now(),
+        },
+      },
+    }
+  );
+  res.redirect(entry.redirectURL);
+});
+
+app.listen(PORT, () => console.log(`Server Started at http://localhost:${PORT}`));
